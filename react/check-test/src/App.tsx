e46339b8dba9f5649d67ebb26e1181456cf0eac4
Paloma <paloma.aubeau@gmail.com>
2024-08-26 10:02:15 +0200

    chore: change architecture into javascript test. React task1 done

---
+++ react/check-test/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name="Bryan Elpitiya" />
+    </div>
+  );
+}
+
+export default App;
