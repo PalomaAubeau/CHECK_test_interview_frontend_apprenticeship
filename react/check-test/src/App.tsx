9ae20bd84fa5c9eefbd483271bf1c83d57116628
Paloma <paloma.aubeau@gmail.com>
2024-08-26 11:08:35 +0200

    chore:: clean react file architecture

--- react/check-test/src/App.tsx
+++ react/check-test/src/App.tsx
@@ -1,16 +1,16 @@
-import "./App.css";
 import Greeting from "./components/Greeting";
-import Counter from "./components/counter/Counter";
-import NameList from "./components/nameList/NameList";
+import Counter from "./components/Counter";
+import NameList from "./components/NameList";
 
-function App() {
+const App = () => {
+  const names = ["Alice", "Bob", "Charlie", "David"];
   return (
     <div className="App">
       <Greeting name="Bryan Elpitiya" />
       <Counter />
-      <NameList names={["Alice", "Bob", "Charlie", "David"]} />
+      <NameList names={names} />
     </div>
   );
-}
+};
 
 export default App;
--- react/check-test/src/components/nameList/index.tsx
+++ react/check-test/src/components/nameList/index.tsx
@@ -1,4 +1,5 @@
 import "./style.css";
+
 const NameList = ({ names }: { names: string[] }) => {
   return (
     <ul className="name-list">
